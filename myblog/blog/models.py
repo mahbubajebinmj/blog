a57799f238503ab3af904c7e15b6dcf95200ffc6
mahbubajebinmj <mahbubajebin.mj.24@gmail.com>
2023-09-24 20:43:17 +0600

    new

---
+++ myblog/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from connection import db 
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
